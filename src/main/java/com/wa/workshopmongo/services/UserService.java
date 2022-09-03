a228160a3724a9e534aa365247951c6d9c1801bf
Wagner Alves <guinn83@hotmail.com>
2022-09-03 17:57:16 -0300

    UserById + Exceções

--- src/main/java/com/wa/workshopmongo/services/UserService.java
+++ src/main/java/com/wa/workshopmongo/services/UserService.java
@@ -1,13 +1,14 @@
 package com.wa.workshopmongo.services;
 
 import com.wa.workshopmongo.domain.User;
-import com.wa.workshopmongo.dto.UserDTO;
 import com.wa.workshopmongo.repositories.UserRepository;
+import com.wa.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService implements Serializable {
@@ -17,5 +18,10 @@ public class UserService implements Serializable {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> user = repository.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
