297b32204ad44d489982300d41e161ddc9900fdd
Wagner Alves <guinn83@hotmail.com>
2022-09-03 19:50:15 -0300

    Entity Post com User aninhado

--- src/main/java/com/wa/workshopmongo/domain/Post.java
+++ src/main/java/com/wa/workshopmongo/domain/Post.java
@@ -1,24 +1,31 @@
 package com.wa.workshopmongo.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+@Document(collection = "post")
 public class Post implements Serializable {
 
+    @Id
     private String id;
     private Date date;
     private String title;
     private String body;
+    private User author;
 
     public Post() {
     }
 
-    public Post(String id, Date date, String title, String body) {
+    public Post(String id, Date date, String title, String body, User author) {
         this.id = id;
         this.date = date;
         this.title = title;
         this.body = body;
+        this.author = author;
     }
 
     public String getId() {
@@ -51,6 +58,14 @@ public class Post implements Serializable {
 
     public void setBody(String body) {
         this.body = body;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
     }
 
     @Override
